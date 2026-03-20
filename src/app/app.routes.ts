f8b20d1f7885a97fc22f5bd811ac57207f5ae57e
maradiaga2001 <113129283+maradiaga2001@users.noreply.github.com>
2026-03-20 11:27:11 -0600

    AU006: Boceto inicial de vistas (usuario, admin y formulario)
    Diseño de las vistas en Angular correspondientes a las historias de usuario AU006, PR001, sin funcionalidad solamente trabajo de UX

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard';
+import { LoanRequestComponent } from './pages/loan-request/loan-request';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'usuario', pathMatch: 'full' },
+  { path: 'usuario', component: UserDashboardComponent },
+  { path: 'admin', component: AdminDashboardComponent },
+  { path: 'solicitar-prestamo', component: LoanRequestComponent }
+];
