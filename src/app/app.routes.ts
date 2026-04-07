261119e303a45fd8622851691af7e3a30fd0199e
Sully-2403 <xavi.diaz.j@gmail.com>
2026-04-07 12:54:16 -0600

    corregir app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { ActiveLoansPage } from './features/loans/components/active-loans/active-loans';
-import { HistorialPage } from './features/admin/pages/historial-page/historial-page';
-import { ConfiguracionesPage } from './features/admin/pages/configuraciones-page/configuraciones-page';
 import { UserDashboardComponent } from './features/dashboard/components/user-dashboard/user-dashboard';
-import { AdminDashboardComponent } from './features/admin/components/admin-dashboard/admin-dashboard';
 
 import { LoanRequestComponent } from './features/loans/components/loan-request/loan-request';
 import { HistorialPageUsuario } from './features/history/components/historial-page-usuario/historial-page-usuario';
 import { ConfiguracionesPageUsuario } from './features/settings/components/configuraciones-page-usuario/configuraciones-page-usuario';
+
+import { HistorialPage } from './features/history/components/historial-page/historial-page';
+import { ConfiguracionesPage } from './features/settings/components/configuraciones-page/configuraciones-page';
 
 export const routes: Routes = [
     {
@@ -43,19 +43,9 @@ export const routes: Routes = [
     },
     { path: '', redirectTo: 'usuario', pathMatch: 'full' },
     { path: 'usuario', component: UserDashboardComponent },
-    { path: 'admin', component: AdminDashboardComponent },
-    { path: 'solicitar-prestamo', component: LoanRequestComponent }
-];
+    { path: 'solicitar-prestamo', component: LoanRequestComponent },
 
-
-
-import { PrestamosActivosAdminPage } from './features/prestamos/pages/prestamos-activos-admin-page/prestamos-activos-admin-page';
-import { HistorialPage } from './features/history/components/historial-page/historial-page';
-import { ConfiguracionesPage } from './features/settings/components/configuraciones-page/configuraciones-page';
-
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'usuario/solicitar-prestamo', pathMatch: 'full' },
+    { path: '', redirectTo: 'usuario/solicitar-prestamo', pathMatch: 'full' },
 
   {
     path: 'usuario',
@@ -70,7 +60,6 @@ export const routes: Routes = [
   {
     path: 'admin',
     children: [
-      { path: 'prestamos-activos', component: PrestamosActivosAdminPage },
       { path: 'historial', component: HistorialPage },
       { path: 'configuraciones', component: ConfiguracionesPage }
     ]
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/components/navbar/navbar';
+import { UserNavigationComponent } from './layout/components/sidebar/user-navigation/user-navigation';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
